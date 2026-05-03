d9ba0d8964b2bc5e0982a74b889548c99f3ae567
csreesan <csreesan.dev@gmail.com>
2026-05-03 11:12:59 -0400

    fix markdown preview

--- init.lua
+++ init.lua
@@ -130,9 +130,17 @@ require("lazy").setup({
   { "ap/vim-css-color" },
 
   -- Markdown preview
-  { "iamcco/markdown-preview.nvim",
-    build = "cd app && npx --yes yarn install",
-    ft = "markdown",
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_echo_preview_url = 1
+    end,
   },
 
   -- Tagbar
